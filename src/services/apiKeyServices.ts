430f7982fbcdc9dfc565c511c2ecac77692e2bb8
Celso <celsolnv27@gmail.com>
2022-04-19 23:47:49 -0300

    Preparando revisão de código

--- src/services/apiKeyServices.ts
+++
@@ -1,9 +0,0 @@
-import * as companyRepo from "../repositories/companyRepository.js";
-import * as errorUtils from "../utils/errorUtils.js";
-
-export async function validate(apiKey: string) {
-  const validation = await companyRepo.findByApiKey(apiKey);
-  if (!validation) {
-    throw errorUtils.authError("API key doesn't belong to any company");
-  }
-}
